044e102ec40c9fc7c0e63660edbb71f3fc58112d
Felix Haeberle <f.haeberle@tojio.com>
2018-12-07 15:36:11 +0100

    add images and setup gulfile for presentation

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var grep = require('gulp-grep');
 var changed = require('gulp-changed');
 var del = require('del');
 var sourcemaps = require('gulp-sourcemaps');
-/*var minifyImg = require('gulp-imagemin');*/ // TODO 1
+var imagemin = require('gulp-imagemin'); // TODO 1 : Install with 'npm install gulp-imagemin' and import it
 var sass = require('gulp-sass');
 
 var paths = {
@@ -15,15 +15,35 @@ var paths = {
       images: 'build/images'
     }
   },
-  images: 'images/*.{JPG,jpg,png,gif,svg}',
-  sass: 'sass/*.scss',
+  images: 'images/**/*.{JPG,jpg,png,gif,svg}',
+  sass: 'sass/**/*.scss',
 };
 
-// TODO 2
+// TODO 2 : Configure imagemin
 
 /*
 gulp.task('images', function () {
   return gulp.src(paths.images)
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: false
+          },
+          {
+            cleanupIDs: false
+          }
+        ]
+      })
+    ]))
     .pipe(gulp.dest(paths.dirs.build.images));
 });
 */
@@ -44,7 +64,7 @@ gulp.task('sass', function () {
 
 // TODO 3
 
-gulp.task('app', gulp.parallel(/*'images',*/ 'sass'));
+gulp.task('app', gulp.parallel(/*'images',*/ 'sass')); // TODO 3 : Execute imagemin
 gulp.task('all', gulp.parallel('app'));
 gulp.task('build', gulp.series('all'));
 
